27f387618b1d541a2351bfd47e8616242de318a6
iknowright <changchaishi@gmail.com>
2018-10-29 09:51:09 +0800

    get bucket with desired format

--- denguebucket/views.py
+++ denguebucket/views.py
@@ -4,20 +4,32 @@ from django.shortcuts import render
 
 from .models import Bucket, BucketRecord, BucketStatistics, DengueBucket
 from .serializers import BucketSerializer, BucketRecordSerializer, BucketStatisticsSerializer, DengueBucketSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response 
 
-from  rest_framework.decorators import action
+from rest_framework.decorators import action, list_route, detail_route
 
 
 import twd97
+import json
 from datetime import datetime, timedelta
+from django.http import HttpResponse
 class BucketViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
     serializer_class = BucketSerializer
     queryset = Bucket.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        buckets = Bucket.objects.all()
+        bucket_dict = dict()
+        for bucket in buckets:
+            bucket_dict[bucket.id] = {
+                'lng': bucket.lng,
+                'lat': bucket.lat
+            }
+        return HttpResponse(json.dumps(bucket_dict), content_type="application/json")
 
 class BucketRecordViewSet(viewsets.ModelViewSet):
     """
