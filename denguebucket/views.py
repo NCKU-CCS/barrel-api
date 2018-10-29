56e9b5a6e29aa593188697398ef52460eebbb09e
iknowright <changchaishi@gmail.com>
2018-10-29 12:58:48 +0800

    bucket record done

--- denguebucket/views.py
+++ denguebucket/views.py
@@ -6,14 +6,13 @@ from .models import Bucket, BucketRecord, BucketStatistics, DengueBucket
 from .serializers import BucketSerializer, BucketRecordSerializer, BucketStatisticsSerializer, DengueBucketSerializer
 from rest_framework import viewsets, status
 from rest_framework.response import Response 
-
 from rest_framework.decorators import action, list_route, detail_route
-
+from django.http import HttpResponse
 
 import twd97
 import json
 from datetime import datetime, timedelta
-from django.http import HttpResponse
+
 class BucketViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
@@ -46,7 +45,44 @@ class BucketRecordViewSet(viewsets.ModelViewSet):
     queryset = BucketRecord.objects.all()
     serializer_class = BucketRecordSerializer
 
+    def get_queryset(self):
+        hasParams = bool(self.request.query_params)
+        if hasParams is False:
+            queryset = BucketRecord.objects.all()
+            return queryset
+        else:
+            start = self.request.query_params.get('start')
+            print (start)
+            if start is None:
+                start = (datetime.now() - timedelta(days=7)).strftime("%Y-%m-%d")
+            end = self.request.query_params.get('end')
+            print (end)
+            if end is None:
+                end = datetime.now().strftime("%Y-%m-%d")
+            county = self.request.query_params.get('county')
+            print (county)
+            if county is None:
+                county = '台南'
+            try:
+                queryset = BucketRecord.objects.filter(
+                    investigate_date__lte=end
+                    ).filter(
+                    investigate_date__gte=start
+                    ).filter(county=county)
+            except:
+                return HttpResponse(status=400)
 
+            town = self.request.query_params.get('town')
+            print (town)
+            village = self.request.query_params.get('village')
+            print (village)
+
+            if town is not None:
+                queryset = queryset.filter(town=town)
+                if village is not None:
+                    queryset = queryset.filter(village=village)
+            
+            return queryset
 
 class BucketStatisticsViewSet(viewsets.ModelViewSet):
     """
