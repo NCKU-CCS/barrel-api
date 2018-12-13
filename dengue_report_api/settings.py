868e813d40e0522f3582cbaf4373a470be8bbfb7
iknowright <changchaishi@gmail.com>
2018-12-13 16:14:57 +0800

    work finw with aws postgres in local host

--- dengue_report_api/settings.py
+++ dengue_report_api/settings.py
@@ -80,10 +80,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'denguebucket',
-        'USER': 'denguebucketuser',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+        'USER': 'denguebucketuser2018',
+        'PASSWORD': 'denguebucketpassword2018',
+        'HOST': 'denguebucket-db.ctbcfrbnvgi6.us-east-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
